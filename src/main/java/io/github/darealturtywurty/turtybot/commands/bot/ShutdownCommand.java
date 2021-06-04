abbeea457f8088e91d623277c21fd4f4f3abfb75
TurtysProductions <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-06-04 17:04:01 +0100

    Update ShutdownCommand.java
    Insert "going offline" message

--- src/main/java/io/github/darealturtywurty/turtybot/commands/bot/ShutdownCommand.java
+++ src/main/java/io/github/darealturtywurty/turtybot/commands/bot/ShutdownCommand.java
@@ -1,21 +1,31 @@
 package io.github.darealturtywurty.turtybot.commands.bot;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import io.github.darealturtywurty.turtybot.commands.core.CommandContext;
 import io.github.darealturtywurty.turtybot.commands.core.IGuildCommand;
 import io.github.darealturtywurty.turtybot.util.BotUtils;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 public class ShutdownCommand implements IGuildCommand {
 
 	@Override
 	public void handle(CommandContext ctx) {
-		ctx.getChannel().sendMessage("Shutting down bot!").queue();
-		ctx.getMessage().delete().queue();
-		Logger.getGlobal().log(Level.WARNING, "Shutting down bot!");
-		BotUtils.shutdownApplication(ctx.getJDA());
-		return;
+		ctx.getJDA().getGuilds().forEach(guild -> {
+			List<TextChannel> channels = guild.getTextChannels().stream()
+					.filter(channel -> channel.getName().contains("general")).collect(Collectors.toList());
+			if (!channels.isEmpty())
+				channels.get(0).sendMessage("I am now going offline for maintenance. Apologies for any inconveniences!")
+						.queue();
+		});
+		ctx.getMessage().delete().queue(deletion -> {
+			Logger.getGlobal().log(Level.WARNING, "Shutting down bot!");
+			BotUtils.shutdownApplication(ctx.getJDA());
+		});
+
 	}
 
 	@Override
