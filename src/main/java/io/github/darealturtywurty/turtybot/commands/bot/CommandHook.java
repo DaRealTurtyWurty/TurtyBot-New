de17611be6c0fc8057c10cd320945df4a27ea4cb
TurtysProductions <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-06-04 17:03:02 +0100

    Update CommandHook.java
    Include sauce.
    Fix some errors
    Include guild ready event

--- src/main/java/io/github/darealturtywurty/turtybot/commands/bot/CommandHook.java
+++ src/main/java/io/github/darealturtywurty/turtybot/commands/bot/CommandHook.java
@@ -1,12 +1,16 @@
 package io.github.darealturtywurty.turtybot.commands.bot;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.egit.github.core.Gist;
@@ -14,11 +18,15 @@ import org.eclipse.egit.github.core.GistFile;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.GistService;
 
+import io.github.darealturtywurty.turtybot.TurtyBot;
 import io.github.darealturtywurty.turtybot.commands.core.CommandManager;
 import io.github.darealturtywurty.turtybot.util.BotUtils;
 import io.github.darealturtywurty.turtybot.util.Constants;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class CommandHook extends ListenerAdapter {
@@ -33,10 +41,17 @@ public class CommandHook extends ListenerAdapter {
 
 	@Override
 	public void onGuildMessageReceived(final GuildMessageReceivedEvent event) {
-		User user = event.getAuthor();
+		var user = event.getAuthor();
 
 		if (event.getMessage().getContentRaw().contains("discord.gg") && !BotUtils.isBotOwner(user)) {
 			event.getMessage().delete().queue();
+			return;
+		}
+
+		// Sauce.
+		// TODO: Delete this if you are hosting the bot yourself.
+		if (event.getMessage().getContentRaw().contains(BotUtils.getPrefixFromGuild(event.getGuild()) + "sauce")) {
+			sauce(event.getMessage());
 			return;
 		}
 
@@ -51,13 +66,13 @@ public class CommandHook extends ListenerAdapter {
 					if (Constants.FILE_EXTENSIONS.contains(attachment.getFileExtension())) {
 						validTextFiles.put(attachment.getFileName(), stream);
 					}
-				} catch (Exception e) {
-					throw new IllegalArgumentException(e);
+				} catch (Exception ex) {
+					throw new IllegalArgumentException(ex);
 				}
 			});
 
 			if (!validTextFiles.isEmpty()) {
-				validTextFiles.forEach((fileName, attachment) -> hostFileOnline(attachment, fileName, (text) -> event
+				validTextFiles.forEach((fileName, attachment) -> hostFileOnline(attachment, fileName, text -> event
 						.getChannel()
 						.sendMessage(event.getAuthor().getAsMention() + " Here, I put that file into a gist for you."
 								+ "\nWe prefer you to not upload files on this server, so please upload it on a paste site!\n<"
@@ -68,30 +83,6 @@ public class CommandHook extends ListenerAdapter {
 			}
 		}
 
-		// Handle showcases
-		if (event.getChannel().getName().equalsIgnoreCase("showcases")) {
-			boolean hasURL = false;
-			for (String item : event.getMessage().getContentRaw().split("\\s+")) {
-				if (Constants.URL_PATTERN.matcher(item).matches())
-					hasURL = true;
-			}
-
-			if (hasURL) {
-				event.getMessage().addReaction("⭐").queue();
-				return;
-			}
-
-			if (event.getMessage().getAttachments().isEmpty()) {
-				event.getMessage().delete().queue();
-			} else if (!(event.getMessage().getAttachments().get(0).isImage()
-					|| event.getMessage().getAttachments().get(0).isVideo())) {
-				event.getMessage().delete().queue();
-			} else {
-				event.getMessage().addReaction("⭐").queue();
-			}
-			return;
-		}
-
 		if (user.isBot() || event.isWebhookMessage()
 				|| !event.getMessage().getContentRaw().startsWith(BotUtils.getPrefixFromGuild(event.getGuild()))) {
 			return;
@@ -100,25 +91,53 @@ public class CommandHook extends ListenerAdapter {
 		this.commandManager.handle(event);
 	}
 
+	@Override
+	public void onGuildReady(GuildReadyEvent event) {
+		super.onGuildReady(event);
+		event.getGuild().loadMembers();
+		List<TextChannel> channels = event.getGuild().getTextChannels().stream()
+				.filter(channel -> channel.getName().contains("general")).collect(Collectors.toList());
+		if (!channels.isEmpty())
+			channels.get(0)
+					.sendMessage(
+							"I am now online. For more information on my commands, visit this server's bot channel and use `"
+									+ BotUtils.getPrefixFromGuild(event.getGuild()) + "help`.")
+					.queue();
+	}
+
 	private void hostFileOnline(InputStream attachment, String fileName, Consumer<String> callback) {
 		try {
-			String text = IOUtils.toString(attachment, StandardCharsets.UTF_8.name());
+			var text = IOUtils.toString(attachment, StandardCharsets.UTF_8.name());
 			if (text.isBlank())
 				return;
-			Gist gist = new Gist();
+			var gist = new Gist();
 
-			GistFile gistFile = new GistFile();
+			var gistFile = new GistFile();
 			gistFile.setContent(text);
 			gist.setFiles(Collections.singletonMap(fileName, gistFile));
 			gist.setDescription("Hello World!");
 
-			GistService gistService = new GistService();
+			var gistService = new GistService();
 			gistService.getClient().setOAuth2Token(this.githubToken);
 			gist = gistService.createGist(gist);
 
 			callback.accept(gist.getHtmlUrl());
-		} catch (IOException e) {
-			callback.accept("Error: " + e.getMessage());
+		} catch (IOException ex) {
+			callback.accept("Error: " + ex.getMessage());
 		}
+	}
+
+	private void sauce(Message message) {
+		message.getAuthor().openPrivateChannel().queue(channel -> {
+			try {
+				InputStream stream = TurtyBot.class.getResourceAsStream("/link.sauce");
+				var reader = new BufferedReader(new InputStreamReader(stream));
+				channel.sendMessage(reader.readLine()).queue();
+			} catch (IOException | InsufficientPermissionException | IllegalArgumentException
+					| UnsupportedOperationException e) {
+				Constants.LOGGER.warning("There was an issue sending a user the sauce. " + e.getLocalizedMessage());
+			}
+		});
+		message.delete().queue();
 	}
 }
