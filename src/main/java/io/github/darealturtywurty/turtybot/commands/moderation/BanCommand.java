e34350aaf67ad594cca8303d1706fe5623fc89e8
TurtysProductions <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-06-04 17:11:30 +0100

    Create BanCommand.java

--- src/main/java/io/github/darealturtywurty/turtybot/commands/moderation/BanCommand.java
+++ src/main/java/io/github/darealturtywurty/turtybot/commands/moderation/BanCommand.java
@@ -1,51 +1,57 @@
 package io.github.darealturtywurty.turtybot.commands.moderation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Color;
 import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nullable;
 
 import io.github.darealturtywurty.turtybot.commands.core.CommandContext;
 import io.github.darealturtywurty.turtybot.commands.core.IGuildCommand;
+import io.github.darealturtywurty.turtybot.util.BotUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.internal.utils.tuple.Pair;
 
 public class BanCommand implements IGuildCommand {
 	@Override
 	public void handle(CommandContext ctx) {
-		Guild guild = ctx.getGuild();
-		Message message = ctx.getMessage();
-		String strToBan = ctx.getArgs()[0];
+		final var guild = ctx.getGuild();
+		final var message = ctx.getMessage();
+		final String strToBan = ctx.getArgs()[0];
 		if (!strToBan.isBlank() && !strToBan.isEmpty()) {
 			if (ctx.getMember().hasPermission(Permission.BAN_MEMBERS, Permission.KICK_MEMBERS)) {
 				Member toBan = null;
-				if (message.getMentionedMembers(guild).size() > 0) {
-					toBan = message.getMentionedMembers(guild).get(0);
-				} else if (guild.getMembersByEffectiveName(strToBan, true).size() > 0) {
-					toBan = guild.getMembersByEffectiveName(strToBan, false).get(0);
+				try {
+					toBan = message.getMentionedMembers().get(0);
+				} catch (IndexOutOfBoundsException ex) {
+					guild.retrieveMemberById(strToBan).queue();
+					toBan = guild.getMemberById(strToBan);
 				}
 
 				if (toBan == null) {
-					try {
-						toBan = guild.getMemberById(Long.parseLong(strToBan));
-					} catch (IllegalArgumentException e) {
-						String[] nameDiscrim = strToBan.split("#");
-						if (nameDiscrim[0] != null) {
-							if (guild.getMembersByEffectiveName(nameDiscrim[0], false).size() > 0) {
-								toBan = guild.getMembersByEffectiveName(nameDiscrim[0], false).get(0);
-							} else if (nameDiscrim.length > 1) {
-								toBan = guild.getMemberByTag(nameDiscrim[0], nameDiscrim[1]);
-							}
-						}
-					}
+					message.reply("Could not find user: " + strToBan).mentionRepliedUser(false).queue();
+					return;
 				}
-				
-				if(toBan != null) { 
-					toBan.ban(0);
+
+				if (toBan.getIdLong() == ctx.getAuthor().getIdLong()) {
+					message.reply("You cannot ban yourself!").mentionRepliedUser(false).queue();
+					return;
 				}
-				
+
+				if (!ctx.getMember().canInteract(toBan)) {
+					message.reply("You cannot ban this user!").mentionRepliedUser(false).queue();
+					return;
+				}
+
+				var reason = "Unspecified";
+
+				if (ctx.getArgs().length > 1) {
+					reason = String.join(" ", ctx.getArgs()).replace(strToBan, "").trim();
+				}
+
+				banMember(guild, ctx.getMember(), toBan, message, reason);
 				return;
 			}
 
@@ -62,6 +68,20 @@ public class BanCommand implements IGuildCommand {
 		});
 	}
 
+	public static void banMember(Guild guild, Member banner, Member toBan, @Nullable Message toDelete, String reason) {
+		final var bannedEmbed = new EmbedBuilder().setColor(Color.RED).setTitle("You were banned from: " + guild.getName())
+				.setDescription("**Reason**: " + reason + "\n**Banned By**: " + banner.getAsMention());
+		toBan.getUser().openPrivateChannel().queueAfter(5, TimeUnit.SECONDS,
+				channel -> channel.sendMessage(bannedEmbed.build()).queue(msg -> toBan.ban(0, reason).queue()));
+
+		final var banLogEmbed = new EmbedBuilder().setColor(Color.RED).setTitle(toBan.getEffectiveName() + " was banned!")
+				.setDescription("**Reason**: " + reason + "\n**Banned By**: " + banner.getAsMention());
+		BotUtils.getModLogChannel(guild).sendMessage(banLogEmbed.build()).queue();
+
+		if (toDelete != null)
+			toDelete.delete().queue();
+	}
+
 	@Override
 	public String getName() {
 		return "ban";
@@ -75,10 +95,5 @@ public class BanCommand implements IGuildCommand {
 	@Override
 	public boolean isModeratorOnly() {
 		return true;
-	}
-
-	@Override
-	public Pair<Boolean, List<String>> validChannels() {
-		return Pair.of(false, new ArrayList<>());
 	}
 }
