1e0391e467095b3d1958c112ff52324d3a380a48
TurtysProductions <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-06-04 17:12:38 +0100

    Start work on starboard

---
+++ src/main/java/io/github/darealturtywurty/turtybot/data/ShowcaseInfo.java
@@ -0,0 +1,23 @@
+package io.github.darealturtywurty.turtybot.data;
+
+public class ShowcaseInfo {
+
+	public final long originalMessageID;
+	public long starboardMessageID;
+	private int stars;
+
+	public ShowcaseInfo(long messageID) {
+		this.originalMessageID = messageID;
+	}
+
+	public void setStars(int stars) {
+		if (stars < 0)
+			this.stars = 0;
+		else
+			this.stars = stars;
+	}
+	
+	public int getStars() {
+		return this.stars;
+	}
+}
